2532f81c55f435655b72c862c56b2b7ed78cdde2
Toby Chidi <chidiubaezuonu@gmail.com>
2023-02-21 11:00:49 +0100

    new: swipe test page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon"],
+   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon", "nuxt-swiper"],
    tailwindcss: {
       config: {
          darkMode: "class",
---
+++ pages/index.vue
@@ -0,0 +1,125 @@
+<script lang="ts" setup>
+import "@fontsource/numans";
+import draggable from "vuedraggable";
+
+const tasklist: Tasklist = {
+   title: "Tasklist",
+   tasks: [{ task: "Eat food", done: false }],
+};
+
+const notes = useLocalStorage<Array<Note>>("notes", []);
+const tasks = useLocalStorage<Array<Task | Tasklist>>("tasks", []);
+const tasksDone = useLocalStorage<Array<Task | Tasklist>>("tasksDone", []);
+const notesChange = ref(0)
+const tasksChange = ref(0)
+const tasksDoneChange = ref(0)
+const tasklistMode = ref<Tasklist | null>(null)
+
+const activeTab = ref("tasks")
+
+const dragOptions = useDragOptions()
+
+function createNewNote(note: Note) {
+   if (note.note) notes.value.push(note);
+}
+function createNewTask(task: Task) {
+   if (task.task) tasks.value.push(task);
+}
+function createNewTasklist(tasklist: Tasklist) {
+   if (tasklist.tasks) tasks.value.push(tasklist);
+}
+
+function convertToTask(tasklist: Tasklist) {
+   tasklistMode.value = null
+}
+
+function convertToTasklist(task: Task) {
+   tasklistMode.value = {
+      title: "",
+      tasks: task.task ? [task] : []
+   }
+}
+
+function handleNotesAdd(e: any) {
+   const newIndex = e.newIndex
+   const oldNotes = notes.value
+   const movedNote = oldNotes[newIndex] as any
+   if (movedNote.task) {
+      oldNotes[newIndex] = { note: movedNote.task }
+   }
+   notes.value = new Array(...oldNotes)
+   notesChange.value++
+}
+
+function handleTasksAdd(e: any) {
+   const newIndex = e.newIndex
+   const oldTasks = tasks.value
+   const movedTask = oldTasks[newIndex] as any
+   if (movedTask.task) {
+      oldTasks[newIndex] = { ...movedTask, done: false }
+   }
+   if (movedTask.note) {
+      oldTasks[newIndex] = { task: movedTask.note, done: false }
+   }
+   tasks.value = new Array(...oldTasks)
+   tasksChange.value++
+}
+
+function handleTasksDoneAdd(e: any) {
+   const newIndex = e.newIndex
+   const oldTasks = tasksDone.value
+   const movedTask = oldTasks[newIndex] as any
+   if (movedTask.task) {
+      oldTasks[newIndex] = { ...movedTask, done: true }
+   }
+   if (movedTask.note) {
+      oldTasks[newIndex] = { task: movedTask.note, done: true }
+   }
+   tasksDone.value = new Array(...oldTasks)
+   tasksDoneChange.value++
+}
+
+</script>
+<template>
+   <main class="h-full w-full md:max-w-6xl flex-1 overflow-hidden">
+      <div class="w-full h-full flex justify-between md:gap-8 lg:gap-12 transition sm:transform-none"
+         :class="{ 'translate-x-[-100%]': activeTab == 'tasks', 'translate-x-[-200%]': activeTab == 'tasksDone' }">
+         <list-board>
+            <note-item clear-on-enter hide-menu @enter="createNewNote" />
+            <draggable v-model:list="notes" :key="notesChange" item-key="note" v-bind="dragOptions" @add="handleNotesAdd">
+               <template #item="{ element: note }">
+                  <li>
+                     <note-item :note="note" :key="note" />
+                  </li>
+               </template>
+            </draggable>
+         </list-board>
+
+         <list-board>
+            <task-list-item v-if="tasklistMode" :tasklist="tasklistMode" clear-on-enter hide-menu
+               @enter="createNewTasklist" @ctrl-enter="convertToTask" />
+            <task-item v-else clear-on-enter hide-menu @enter="createNewTask" @ctrl-enter="convertToTasklist" />
+            <draggable v-model:list="tasks" :key="tasksChange" item-key="task" v-bind="dragOptions" @add="handleTasksAdd">
+               <template #item="{ element: task }">
+                  <li v-if="task.task || task.tasks">
+                     <task-item v-if="task.task" :task="task" />
+                     <task-list-item v-if="task.tasks" :tasklist="task" />
+                  </li>
+               </template>
+            </draggable>
+         </list-board>
+
+         <list-board>
+            <draggable v-model:list="tasksDone" :key="tasksDoneChange" item-key="task" v-bind="dragOptions"
+               @add="handleTasksDoneAdd">
+               <template #item="{ element: task, }">
+                  <li>
+                     <task-item :task="task" />
+                  </li>
+               </template>
+            </draggable>
+         </list-board>
+      </div>
+   </main>
+   <mobile-nav class="sm:hidden" v-model:active-tab="activeTab" />
+</template>
