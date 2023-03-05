83d561f57bfe6ec790a76f9f6803ce731a4e4fcd
Toby Chidi <chidiubaezuonu@gmail.com>
2023-03-05 20:39:59 +0100

    fix: responsive styles

--- components/ListBoard.vue
+++ components/ListBoard.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="bg-red-100 p-1 overflow-auto overflow-x-hidden flex flex-col flex-1 gap-4 scroll-mr-2">
+   <div class="p-1 overflow-auto overflow-x-hidden flex flex-col flex-1 gap-4 scroll-mr-2">
       <slot />
    </div>
 </template>
