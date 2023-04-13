54e040cc74536b2d3ae65b3c9dc9a486eccecfed
--get <chidiubaezuonu@gmail.com>
2023-04-13 12:13:20 +0100

    update images folder name

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -5,10 +5,10 @@
    <div>
       <h2 class="text-xl">Gallery</h2>
       <div class="flex">
-         <nuxt-img src="/gallery/pexels02.jpg" alt="Discover Nuxt 3" class="h-60 w-60 rounded-full object-cover" />
-         <nuxt-img src="/gallery/pexels01.jpg" alt="Discover Nuxt 3" class="h-80 w-80 rounded-full object-cover" />
-         <nuxt-img src="/gallery/pexels03.jpg" alt="Discover Nuxt 3" class="h-60 w-60 rounded-full object-cover" />
-         <nuxt-img src="/gallery/pexels04.jpg" alt="Discover Nuxt 3" class="h-60 w-60 rounded-full object-cover" />
+         <nuxt-img src="/pics/pexels02.jpg" alt="Discover Nuxt 3" class="h-60 w-60 rounded-full object-cover" />
+         <nuxt-img src="/pics/pexels01.jpg" alt="Discover Nuxt 3" class="h-80 w-80 rounded-full object-cover" />
+         <nuxt-img src="/pics/pexels03.jpg" alt="Discover Nuxt 3" class="h-60 w-60 rounded-full object-cover" />
+         <nuxt-img src="/pics/pexels04.jpg" :modifiers="{ grayscale: true, tint: '#00DC82' }" alt="Discover Nuxt 3" class="h-60 w-60 rounded-full object-cover" />
       </div>
    </div>
 </template>
