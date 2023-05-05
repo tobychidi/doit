cef0b31ce7d71b5cad17c627828cc8a5b01a8b02
tobychidi <chidiubaezuonu@gmail.com>
2023-05-05 17:23:19 +0100

    some updates

--- components/ToggleLogo.vue
+++ components/ToggleLogo.vue
@@ -5,15 +5,11 @@ import logoDark from "~/assets/doit_logo_dark.svg";
 </script>
 <template>
    <div>
-      <button v-show="$colorMode.preference == 'dark'" @click="$colorMode.preference = 'light'">
-         <span>
-            <img :src="logo" class="h-8 sm:h-10" alt="Doit">
-         </span>
-      </button>
-      <button v-show="$colorMode.preference == 'light'" @click="$colorMode.preference = 'dark'">
-         <span>
-            <img :src="logoDark" class="h-8 sm:h-10" alt="Doit">
-         </span>
-      </button>
+      <div v-show="$colorMode.preference == 'dark'" @click="$colorMode.preference = 'light'">
+         <img :src="logo" class="h-8 sm:h-10" alt="Doit">
+      </div>
+      <div v-show="$colorMode.preference == 'light'" @click="$colorMode.preference = 'dark'">
+         <img :src="logoDark" class="h-8 sm:h-10" alt="Doit">
+      </div>
    </div>
 </template>
